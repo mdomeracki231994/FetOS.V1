618fef88a9c629f89f0e906001e132dbda51c212
mdomeracki231994 <domerackimark@gmail.com>
2024-08-08 22:08:31 -0400

    Updating calendar

--- fet_os_calendar/views.py
+++ fet_os_calendar/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render
 
 from fetos_events.models import Events
+from fetos_jobs.models import FetosJob
 
 
 def calendar_page(request):
     fet_os_events = Events.objects.all()
-    print(fet_os_events.values())
+    fet_os_jobs = FetosJob.objects.all()
 
     context = {
         'fet_os_events': fet_os_events,
-
+        'fet_os_jobs': fet_os_jobs,
     }
     return render(request, 'fet_os_calendar/my_calendar.html', context)
